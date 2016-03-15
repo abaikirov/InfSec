ce60e841a88f8100bf62b7aafa0818c9ac93e254
Абакиров Абай Ильясович <abaikirov@outlook.com>
2016-03-15 22:44:58 +0600

    b

--- InfSec/Program.cs
+++ InfSec/Program.cs
@@ -11,7 +11,7 @@ namespace InfSec
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-
+            Console.ReadKey();
             Console.ReadKey();
         }
     }
